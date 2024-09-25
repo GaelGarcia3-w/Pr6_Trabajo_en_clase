a83886fd2e0a2dfab09076ee081c218f8a0d29ae
Galletas <a.23308051281182@cbtis128.edu.mx>
2024-09-24 19:58:16 -0600

    Pr6_Trabajo_en_clase

---
+++ Pr6_Trabajo_en_clase.py
@@ -0,0 +1,10 @@
+print(" ")
+print("Edgar Gael Garcia Camacho :Pr6_Trabajo_en_clase")
+print(" ")
+num1=int(input("En que numero estoy pensando ???" ))#Te dice que en que numero esta pensando
+
+if num1==5:#se usa "if" para que el usuario pueda adivinar que numero se esta pensando
+  print("Estoy pensando en este numero felicidades")# te manda un mesaje si adivinaste el numero
+
+else:
+        print("No estoy pensando en este numero intentalo otra vez")#te manda un mensaje si no adivinaste el numero
